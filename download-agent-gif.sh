5cf5de301e88645315e1286d082368ddc5c97812
Debayan Biswas <debayanbiswas1111@gmail.com>
2026-02-13 03:50:21 +0530

    Updated Readme

--- download-agent-gif.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Backup: Download dark-theme coding GIF for Experience sections (README uses assets/ittiam.gif).
-# Run from repo root: ./download-agent-gif.sh
-# Source: Coding The Matrix - dark/green theme (happyantsstudio)
-# https://giphy.com/gifs/glitch-matrix-cat-wwg1suUiTbCY8H8vIA
-curl -L -o assets/ittiam.gif "https://media.giphy.com/media/wwg1suUiTbCY8H8vIA/giphy.gif"
-echo "Done. Saved to assets/ittiam.gif"
